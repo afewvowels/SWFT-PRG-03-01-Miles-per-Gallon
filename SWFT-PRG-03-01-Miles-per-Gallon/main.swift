3fb619275cf451d0c68da8a1ec5ad0d5a342a9c4
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-11 12:44:51 -0400

    Initial Commit

---
+++ SWFT-PRG-03-01-Miles-per-Gallon/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SWFT-PRG-03-01-Miles-per-Gallon
+//
+//  Created by Keith Smith on 10/11/17.
+//  Copyright © 2017 Keith Smith. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
